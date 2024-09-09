d6b8de31528507d957777a49e7ca6b016314934f
Vlad Kramskoy <vkto5609@gmail.com>
2024-09-09 20:18:52 +0300

    Add data model for cart

---
+++ FakeNFT/Models/Cart/CartNft.swift
@@ -0,0 +1,15 @@
+//
+//  CartNft.swift
+//  FakeNFT
+//
+//  Created by Vladislav Kramskoy on 09.09.2024.
+//
+
+import UIKit
+
+struct CartNft {
+    let name: String
+    let image: UIImage
+    let rating: Int
+    let price: Float
+}
